b27928862b81fc0feb4873c8fe415b1ac88a29a0
sashachiu <sashachiu@sashachiude-macbook-pro.local>
2018-11-17 19:25:32 +0800

    Initial Commit

---
+++ Taihu Brewing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Taihu Brewing
+//
+//  Created by sashachiu on 2018/11/17.
+//  Copyright © 2018 sashachiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
